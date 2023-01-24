86dd2623453bf8d09e469b35904a0e014a6730b8
Neider Hernández <neiderkk@gmail.com>
2023-01-24 07:08:46 -0500

    agregando interactividad en el titulo principal

--- src/components/MainLetter.jsx
+++ src/components/MainLetter.jsx
@@ -1,12 +1,19 @@
 export const MainLetter = () => {
   return (
     <>
-   <div className="main-latter" >
-        <center className="texts" >
-            <h1 className="name title" >ESCUELA DEPORTIVA DE KARATE DO</h1>
-            <h2 className="name subtitle" >BYAKKO YAMA</h2>
-        </center>
-   </div>
+      <div className="main-content">
+        <div className="concept concept-two">
+          <div className="main-latter" >
+            <center className="texts"  >
+              <div className="hover">
+                <h1 className="name title" >ESCUELA DEPORTIVA DE KARATE DO</h1>
+                <h1 h2 className="name subtitle" >BYAKKO YAMA</h1>
+              </div>
+            </center>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
